206d937c7a646291a426cad37ec2171f12f67bcc
Brunostar <tanjubrunostar0@gmail.com>
2021-12-30 00:18:49 +0100

    at this stage, everything works perfectly

--- src/redux/cakes/cakeReducer.js
+++ src/redux/cakes/cakeReducer.js
@@ -1,12 +1,12 @@
-import { buyCake } from "./cakeActions"
+import { BUY_CAKE } from "./cakeTypes"
 
 const initialState = {
     numOfCakes: 10
 }
 
 const cakeReducer = (state = initialState, action) => {
-    switch(action) {
-        case buyCake: return {
+    switch(action.type) {
+        case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1,
         }
